854a89bebf525bd47069b328ccf3b2b7851ca954
Biggie Emmanuel <bigolu96@gmail.com>
2017-05-10 19:37:03 -0400

    add script to fix graph data + other stuff

--- mst/impl2.cu
+++ mst/impl2.cu
@@ -33,12 +33,20 @@ void printEdges(std::vector<edge> edges){
 
 int getNumVertices(std::vector<edge> edges){
     int max = -1;
+
+    // vertices will be numbered starting from 0 or 1
+    bool verticesStartFromZero = false;
+
     for(edge e : edges){
         int tmp = std::max(e.src, e.dest);
         max = std::max(max, tmp);
+
+        if(e.src == 0){
+            verticesStartFromZero = true;
+        }
     }
 
-    return max + 1;
+    return verticesStartFromZero ? max + 1 : max;
 }
 
 void writeAnswer(int *output, int len){
@@ -66,11 +74,73 @@ __global__ void cudaInitIntArray(int *a, int len, int val){
 }
 
 struct vertex{
-  unsigned int start;
+  int start;
   unsigned int len;
+  int successor;
 };
 
 /****** END UTIL METHODS ******/
+
+bool done(vertex *v, int vlen){
+    vertex tmp;
+    cudaMemcpy(&tmp, v, sizeof(vertex), cudaMemcpyDeviceToHost);
+    int successor = tmp.successor;
+    for(int i = 1; i < vlen; i++){
+        cudaMemcpy(&tmp, &v[i], sizeof(vertex), cudaMemcpyDeviceToHost);
+        if(successor != tmp.successor){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+__global__ void
+findMins(vertex *v, edge *e, int *inMst, int vlen, int elen){
+    int totalThreads = gridDim.x * blockDim.x;
+    int totalWarps = (totalThreads % 32 == 0) ?  totalThreads / 32 : totalThreads / 32 + 1;
+    int threadId = blockDim.x * blockIdx.x + threadIdx.x;
+    int warpId = threadId / 32;
+    int laneId = threadId % 32;
+    int load = (vlen % totalWarps == 0) ? vlen / totalWarps : vlen / totalWarps + 1;
+    int beg = load * warpId;
+    int end = (vlen < beg + load) ? vlen : beg + load;
+    beg = beg + laneId;
+
+    for(int i = beg; i < end; i += 32){
+        vertex cur = v[i];
+        int min = INT_MAX;
+        int minIndex = elen;
+        for(int j = cur.start; j < (cur.start + cur.len); j++){
+            int w = e[j].weight;
+            cur.successor = ((min <= w) * cur.successor) + ((w < min) * e[j].dest);
+            min = ((min <= w) * min) + ((w < min) * w);
+            minIndex = ((min <= w) * minIndex) + ((w < min) * j);
+        }
+        inMst[minIndex] = 1;
+        e[minIndex].weight = INT_MAX;
+    }
+}
+
+__global__ void
+setSuccessors(vertex *v, int vlen){
+    int totalThreads = gridDim.x * blockDim.x;
+    int totalWarps = (totalThreads % 32 == 0) ?  totalThreads / 32 : totalThreads / 32 + 1;
+    int threadId = blockDim.x * blockIdx.x + threadIdx.x;
+    int warpId = threadId / 32;
+    int laneId = threadId % 32;
+    int load = (vlen % totalWarps == 0) ? vlen / totalWarps : vlen / totalWarps + 1;
+    int beg = load * warpId;
+    int end = (vlen < beg + load) ? vlen : beg + load;
+    beg = beg + laneId;
+
+    for(int i = beg; i < end; i += 32){
+        vertex cur = v[i];
+        while(v[cur.successor].successor != cur.successor){
+            cur.successor = v[cur.successor].successor;
+        }
+    }
+}
 
 void mst(std::vector<edge> * edgesPtr, int blockSize, int blockNum){
     setTime();
@@ -83,16 +153,20 @@ void mst(std::vector<edge> * edgesPtr, int blockSize, int blockNum){
 
     // get vertex list
     vertex *vertices;
-    int numVertices = getNumVertices(edgeVector);
-    int vlen = numVertices;
-    vertices = (vertex*)malloc(numVertices * sizeof(vertex));
+    int vlen = getNumVertices(edgeVector);
+    vertices = (vertex*)malloc(vlen * sizeof(vertex));
+    for(int i = 0; i < vlen; i++){
+        vertices[i].start = INT_MAX;
+        vertices[i].successor = i;
+    }
     int prevSrc = -1;
-    int curVertex, start, len = 0;
+    int curVertex = 0;
+    int start = 0;
+    int len = 0;
     for(edge e : *edgesPtr){
-      len++;
       if(prevSrc == -1){
+        curVertex = e.src;
         prevSrc = e.src;
-        continue;
       }
 
       if(prevSrc != e.src){
@@ -100,9 +174,12 @@ void mst(std::vector<edge> * edgesPtr, int blockSize, int blockNum){
         vertices[curVertex].len = len;
         
         start += len;
-        curVertex++;
+        curVertex = e.src;
+        prevSrc = e.src;
         len = 0;
       }
+
+      len++;
     }
     vertices[curVertex].start = start;
     vertices[curVertex].len = len;
@@ -112,6 +189,30 @@ void mst(std::vector<edge> * edgesPtr, int blockSize, int blockNum){
     }
     for(int i = 0; i < elen; i++){
         printf("e%d: src = %d dest = %d weight = %d\n", i, edges[i].src, edges[i].dest, edges[i].weight);
+    }
+
+    int *inMst;
+    cudaMalloc((void**)&inMst, sizeof(int) * elen);
+    cudaInitIntArray<<<blockNum, blockSize>>>(inMst, elen, 0);
+
+    edge *e; 
+    cudaMalloc((void**)&e, sizeof(edge) * elen);
+    cudaMemcpy(e, edgeVector.data(), elen * sizeof(edge), cudaMemcpyHostToDevice);
+
+    vertex *v;
+    cudaMalloc((void**)&v, sizeof(vertex) * vlen);
+    cudaMemcpy(v, vertices, vlen * sizeof(vertex), cudaMemcpyHostToDevice);
+
+    while(!done(v, vlen)){
+        findMins<<<blockSize, blockNum>>>(v, e, inMst, vlen, elen);
+        setSuccessors<<<blockSize, blockNum>>>(v, vlen);
+    }
+
+    for(int i = 0; i < elen; i++){
+        if(readCudaInt(&inMst[i]) == 1){
+            edge tmp = edges[i];
+            printf("IN MST: %d\t%d\n", tmp.src, tmp.dest);
+        }
     }
 
     cudaDeviceProp props; cudaGetDeviceProperties(&props, 0);
